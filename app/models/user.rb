ecc00f344e60b464fc3dae70fd3a0b12e73327dc
dat-hedspi <dat.hedspi@gmail.com>
2017-10-31 10:59:29 +0700

    omniauth with facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,39 @@
 class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable,
+    :trackable, :validatable, :lockable, :omniauthable,
+    omniauth_providers: [:facebook, :google_oauth2]
 
   mount_uploader :avatar, User::AvatarUploader
-  
+
   validates :username, uniqueness: true, length: { in: 6..20 }
 
-  def soft_delete  
-    update_attribute(:deleted_at, Time.current)  
-  end 
+  def soft_delete
+    update_attribute(:deleted_at, Time.current)
+  end
 
-  def active_for_authentication?  
-    super && !deleted_at  
-  end 
+  def active_for_authentication?
+    super && !deleted_at
+  end
 
-  def inactive_message   
-    !deleted_at ? super : :deleted_account  
-  end 
+  def inactive_message
+    !deleted_at ? super : :deleted_account
+  end
+
+  def self.new_with_session params, session
+    super.tap do |user|
+      if data = session["devise.facebook_data"] &&
+        session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+
+  def self.from_omniauth auth
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.username = auth.info.name
+    end
+  end
 end
