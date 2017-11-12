77208c4c3927440bc8b409ec7910a3ef5b5887c2
ngocquyhoang <ngocquyhoang.93@gmail.com>
2017-11-13 01:09:54 +0700

    Completed User account

--- app/assets/javascripts/users/application.js
+++ app/assets/javascripts/users/application.js
@@ -1,13 +1,7 @@
 //= require jquery_ujs
 //= require jquery.remotipart
-//= require semantic-ui
 
-//= require 'mouse.parallax'
 //= require 'pnotify.min'
 
 //= require 'users/dashboard'
-//= require 'users/moment'
 //= require 'users/events'
-
-//= require 'vendor/bootstrap.min'
-//= require 'vendor/bootstrap-datetimepicker'
