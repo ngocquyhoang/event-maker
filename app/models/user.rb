8604364e35d77eed9c178343bd20f8c02816012f
ngocquyhoang <ngocquyhoang.93@gmail.com>
2018-01-05 22:32:13 +0700

    Social login clean ver 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,7 +36,7 @@ class User < ApplicationRecord
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-      user.username = auth.info.name
+      user.name = auth.info.name
     end
   end
 end
